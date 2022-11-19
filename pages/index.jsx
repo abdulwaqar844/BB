0758f58113bd321b8a14a9baaaa073e70314d6e5
abdulwaqar844 <abdulwaqar844@gmail.com>
2022-11-19 15:43:58 +0500

    time component added

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import { auth } from "../lib/firebase";
 import { useQuery } from "@apollo/client";
 import GET_ALL_USER_HABIT from "../lib/apollo/queries/getHabits";
 import Habits from "../components/Habits";
+import TimeAndDate from "../components/TimeAndDate";
 import Modal from "../components/NewHabit";
 import { Context } from "../context";
 
@@ -19,14 +20,11 @@ export default function Home() {
 
   const handleShow = () => setShow(true);
 
-  
-    onAuthStateChanged(auth, (user) => {
-      if (!user) {
-        router.push("/login");
-      }
-   
-    });
- 
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      router.push("/login");
+    }
+  });
 
   const { loading, data } = useQuery(
     GET_ALL_USER_HABIT,
@@ -37,11 +35,14 @@ export default function Home() {
       fetchPolicy: "no-cache",
     }
   );
-  if (loading && state.user.id) return <div className="d-flex justify-content-center">
-  <div className="spinner-border" role="status">
-    <span className="visually-hidden">Loading...</span>
-  </div>
-</div>
+  if (loading && state.user.id)
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
   return (
     <div>
       <Head>
@@ -51,27 +52,31 @@ export default function Home() {
       </Head>
 
       <main className="container">
-        <div>
-          <p className="h3  text-center">Your Habits
-          </p>
+        <div className="row">
+          <div className="col-md-10">    <div>
+            <p className="h3  text-center">Your Habits</p>
+          </div>
+            <div className="d-flex justify-content-center justify-content-center justify-content-md-end px-5 py-2"  >
+              <button className="btn btn-success " onClick={handleShow}>
+                Create Habit
+              </button>
+            </div>
+            {data &&
+              data.habits.map((habit, index) => {
+                return (
+                  <div className=" px-5 py-4" key={index}>
+                    <Habits habit={habit} />
+                  </div>
+                );
+              })}</div>
+          <div className="col-md-2">
+            < TimeAndDate />
+          </div>
         </div>
-        <div
-          className="d-flex justify-content-center justify-content-center
- justify-content-md-end px-5 py-2 "
-        >
-          <button className="btn btn-success " onClick={handleShow}>
-            {" "}
-            Create Habit
-          </button>
-        </div>
-        {data &&
-          data.habits.map((habit, index) => {
-            return (
-              <div className=" px-5 py-4" key={index}>
-                <Habits habit={habit} />
-              </div>
-            );
-          })}
+
+
+
+
         <Modal status={show} HanldeShowModal={HanldeShowModal} />
       </main>
     </div>
