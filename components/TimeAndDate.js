87d8c37b10b5b146e0a53993e60421a2143d3145
abdulwaqar844 <abdulwaqar844@gmail.com>
2022-12-11 21:13:26 +0500

    date component updated

--- components/TimeAndDate.js
+++ components/TimeAndDate.js
@@ -12,24 +12,26 @@ function App() {
   return (
     <div className="container py-5 text-center watch">
       
-  
-      {/* <ClockIcon /> */}
-      <p className="h5 py-2 text-light" >       
+  <div>
+  <p className="h5 py-2 text-light" >       
 
-        {dateState.toLocaleString("en-US", {
-          second: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-          hour12: true,
-        })}
-      </p>
-      <p className="h3 py-3 text-light" >       
-        {dateState.toLocaleDateString("en-GB", {
-          day: "numeric",
-          month: "short",
-          year: "numeric",
-        })}
-      </p>
+{dateState.toLocaleString("en-US", {
+  second: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+})}
+</p>
+<p className="h3 py-3 text-light" >       
+{dateState.toLocaleDateString("en-GB", {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+})}
+</p>
+  </div>
+  <div className="text-info">Double Click on Habit Day to mark as completed</div>
+
     </div>
   );
 }
